41b10aac46049cb6cb1af5c0bf4b664efa6bee31
Piratika <pira112120@gmail.com>
2018-10-08 21:50:19 +0300

    fix ext bug

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  .action((file1, file2) => fs.writeFileSync(`result.${ext}`, genDiff(file1, file2, option.format || 'txt')))
+  .action((file1, file2) => fs.writeFileSync(`result.${option.format}`, genDiff(file1, file2, option.format || 'txt')))
   .parse(process.argv);
 
 if (program.args.length === 0) program.help();
