f2cd1c45600271bff303f555ae486cfe5aa4b212
Piratika <pira112120@gmail.com>
2018-10-13 19:23:30 +0300

    one another fix

--- src/renderers/renderJSON.js
+++ src/renderers/renderJSON.js
@@ -1,17 +1,1 @@
-export default (result) => {
-  const typeChange = {
-    removed: () => 'removed',
-    added: a => ({ hasbeen: 'added', value: a.value }),
-    unchanged: () => {},
-    changed: a => ({ hasbeen: 'changed', valueOld: a.valueOld, valueNew: a.valueNew }),
-    haschildren: (a, f) => f(a.children),
-  };
-
-  const stringifyJSON = obj => obj.reduce((acc, a) => {
-    const value = typeChange[a.type](a, stringifyJSON);
-    acc[a.key] = value;
-    return acc;
-  }, {});
-
-  return `${JSON.stringify(stringifyJSON(result), null, ' ')}\n`;
-};
+export default result => `${JSON.stringify(result, null, ' ')}\n`;
